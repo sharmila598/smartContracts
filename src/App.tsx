be74fe3518e68fe857d504d6e4124809ca5ad076
sharmila <skshakilask67@gmail.com>
2024-12-06 17:16:59 +0530

    integrated wallet with wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import { GrMoney } from "react-icons/gr";
 import { CiSearch } from "react-icons/ci";
+import { useModal } from "connectkit";
+
 const CenteredForm = () => {
+  const { openProfile } = useModal();
   return (
-    <div className="h-screen bg-gray-100 flex items-center justify-center">
+    <div className="h-screen bg-gray-100 flex items-center justify-center relative">
       <div className="w-full max-w-md p-4">
-        <div className="space-y-4">
+        {/* Money button positioned at the top left */}
+        <div className="absolute top-10 left-4 flex ">
+          <button
+            onClick={openProfile}
+            className="border border-gray-300 m-2 p-2 rounded-md flex flex-row place-items-center"
+          >
+            <GrMoney size={24} className="text-black m-2" /> wallet
+          </button>
+        </div>
+
+        <div className="space-y-4 mt-10">
           {/* Full-width input */}
-          <div className="relative">
+          <div className="w-full relative">
             <input
               type="text"
-              placeholder="Enter contact address"
+              placeholder="Search by address"
               className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
             />
             <CiSearch
-              className=" absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
               size={30}
             />
           </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { anvil, mainnet } from "wagmi/chains";
+
 import "./index.css";
 
 import App from "./App";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-// import reportWebVitals from './reportWebVitals';
-const queryclient = new QueryClient();
+import { createConfig, http, WagmiProvider } from "wagmi";
+
+const queryClient = new QueryClient();
+export const config = createConfig(
+  getDefaultConfig({
+    appName: "smartContacts",
+    chains: [mainnet],
+    transports: {
+      [mainnet.id]: http("http://localhost:8545"),
+    },
+    walletConnectProjectId:
+      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+  })
+);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <QueryClientProvider client={queryclient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <ConnectKitProvider>
+          <App />
+        </ConnectKitProvider>
+      </React.StrictMode>
+    </QueryClientProvider>
+  </WagmiProvider>
 );
